1fee880c79ae903b33bc269186e6a23b7cc7edec
javid shabin <javid.main.act@gmail.com>
2024-12-06 00:17:11 +0530

    make update

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -61,7 +61,7 @@ const List = () => {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <span className="loading loading-dots loading-lg bg-orange-400"></span>
+        <span className="text-orange-400">Loading...</span>
       </div>
     );
   }
